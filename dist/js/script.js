ad1b2daaebc52091eb2892625980a9bc97d1bcaf
mrsaddam32 <neolinedev@gmail.com>
2022-04-12 00:19:00 +0700

    add darkmode & fix features

--- dist/js/script.js
+++ dist/js/script.js
@@ -11,10 +11,44 @@ hamburger.addEventListener("click", function () {
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const scrollTop = document.querySelector("#to-top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    scrollTop.classList.remove("hidden");
+    scrollTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    scrollTop.classList.add("hidden");
+    scrollTop.classList.remove("flex");
   }
 };
+
+// Click on outside hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != navMenu && e.target != hamburger) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Darkmode
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// Move toggle mode
+if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
